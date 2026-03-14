74c2b964387808597cdacb84c6607e3f37d37e72
Казенин Владимир <kazeninvladimir@gmail.com>
2026-03-14 17:45:17 +0300

    Fix jackson error: added constructors to os responses

--- ask-core/src/main/java/ru/askorium/core/index/IndexText.java
+++ ask-core/src/main/java/ru/askorium/core/index/IndexText.java
@@ -2,8 +2,10 @@ package ru.askorium.core.index;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class IndexText {
 
--- ask-core/src/main/java/ru/askorium/core/index/IndexVector.java
+++ ask-core/src/main/java/ru/askorium/core/index/IndexVector.java
@@ -2,10 +2,12 @@ package ru.askorium.core.index;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class IndexVector {
 
