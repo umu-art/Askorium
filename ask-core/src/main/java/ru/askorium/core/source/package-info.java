316ae789ba30fb055bd64a5d560be211c5577f5a
Казенин Владимир <kazeninvladimir@gmail.com>
2026-05-06 01:22:30 +0300

    fixs

--- ask-core/src/main/java/ru/askorium/core/source/package-info.java
+++ ask-core/src/main/java/ru/askorium/core/source/package-info.java
@@ -1,4 +1,4 @@
 @org.springframework.modulith.ApplicationModule(
-        allowedDependencies = {"user", "text_processing", "common", "exception", "index", "indexing"}
+        allowedDependencies = {"user", "text_processing", "common", "exception", "index", "encoder"}
 )
 package ru.askorium.core.source;
